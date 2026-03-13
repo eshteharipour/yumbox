4ab5d47c9cd062890292a4f22f8662543142aca2
ShayanEs <shayaneshteharipour@gmail.com>
2026-03-13 13:51:02 +0330

    nlp text processor helper functions and refactor chars
    Preprocessor class now correctly uses parse_html instead of html_to_text

--- yumbox/nlp/__init__.py
+++ yumbox/nlp/__init__.py
@@ -1,18 +1,13 @@
-import html
 import re
 import string
-import unicodedata
-import urllib.parse
 from typing import Literal
 
 import hazm
 import nltk
-import six
-import unicodeblock.sequence
-from lxml import etree
-from parsel import Selector
-from tqdm import tqdm
 
+from yumbox.scraper import parse_html
+
+from .chars import Chars, Unicode, UnicodeRanges
 from .tools import MapRed, defaultname, replace_fromstart
 
 
@@ -78,7 +73,6 @@ class Preprocessor:
             - fa_sent_normalizer
             - en_sent_normalizer
             - parse_html
-            - html_to_text
             - product_title_preprocessor
         Functions to be included:
             - sentence_normalizer
@@ -209,7 +203,7 @@ class Preprocessor:
             return ""
 
         if self.parse_html:
-            s = html_to_text(s)
+            s = parse_html(s)
         if self.remove_uri:
             s = self.uri_remover(s)
         if self.remove_www:
@@ -357,221 +351,3 @@ class SentTokenizer:
 
     def __call__(self, s: str) -> str:
         return self.sent_tokenizer(s)
-
-
-class UnicodeRanges:
-    """
-    References:
-        https://en.wikipedia.org/wiki/Arabic_script_in_Unicode
-        https://en.wikipedia.org/wiki/CJK_Unified_Ideographs
-    """
-
-    english_letters = r"\u0041-\u005A\u0061-\u007A"
-    persian_letters = r"\u0600-\u06FF\u0750-\u077F"
-    numerical_digits = r"\u0030-\u0039"
-    special_chars = "!@#$%^&*()_+{{}}\[\]:;\"'<>,.?/|~`×\u200c=.-]"
-
-    # A-Z and a-z
-    english_letters_ranges = [(0x0041, 0x005B), (0x0061, 0x007B)]
-    # Arabic and Persian ranges
-    persian_letters_ranges = [(0x0600, 0x0700), (0x0750, 0x0780)]
-    # 0-9:;<=>?
-    numerical_digits_ranges = [(0x0030, 0x0040)]
-    specials = [(0x0032, 0x0047), (0x0058, 0x0064), (0x0091, 0x0096), (0x0123, 0x0126)]
-
-    cjk_ranges = [
-        (0x4E00, 0x9FFF),  # CJK Unified Ideographs
-        # (0x3400, 0x4DBF),  # CJK Unified Ideographs Extension A
-        # (0x20000, 0x2A6DF),  # CJK Unified Ideographs Extension B
-        # (0x2A700, 0x2B73F),  # CJK Unified Ideographs Extension C
-        # (0x2B740, 0x2B81F),  # CJK Unified Ideographs Extension D
-        # (0x2B820, 0x2CEAF),  # CJK Unified Ideographs Extension E
-        # (0xF900, 0xFAFF),  # CJK Compatibility Ideographs
-        # (0x2F800, 0x2FA1F),  # CJK Compatibility Ideographs Supplement
-    ]
-
-    @classmethod
-    def regex_from_range(self, ranges: list[tuple[int, int]], negate=False):
-        hex_to_unicode = lambda x: rf"\u{format(x, '04X')}"
-        ranges = [(hex_to_unicode(s), hex_to_unicode(e)) for s, e in ranges]
-        ranges = [f"{s}-{e}" for s, e in ranges]
-        ranges = "".join(ranges)
-        if negate == False:
-            pattern = rf"[{ranges}]"
-        else:
-            pattern = rf"[^ {ranges}]"  # Added space
-        return re.compile(pattern)
-
-    @classmethod
-    def unknown_chars(self):
-        return rf"[^{self.english_letters}{self.persian_letters}{self.numerical_digits}{self.special_chars}"
-
-    @classmethod
-    def regex_cjk_chars(self):
-        return self.regex_from_range(self.cjk_ranges)
-
-    @classmethod
-    def print_unknown_chars(self, words: list):
-        regex = re.compile(self.unknown_chars())
-
-        for word in tqdm(words):
-            if bool(regex.search(word)):
-                print(word)
-
-    @classmethod
-    def print_unicode_range(self, ranges: list[tuple[int, int]]):
-        for start, end in ranges:
-            for codepoint in range(start, end):
-                print(chr(codepoint), end="")
-            print()  # New line after each range
-
-    @classmethod
-    def print_known_ranges(self):
-        print("English Letters:")
-        self.print_unicode_range(self.english_letters_ranges)
-
-        print("\nPersian Letters:")
-        self.print_unicode_range(self.persian_letters_ranges)
-
-        print("\nNumerical Digits:")
-        self.print_unicode_range(self.numerical_digits_ranges)
-
-        print("\nSpecial chars:")
-        self.print_unicode_range(self.specials)
-
-
-class Unicode:
-    # https://www.fileformat.info/info/unicode/category/index.htm
-    # https://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-normalize-in-a-python-unicode-string
-
-    def strip_accents(text: str):
-        """Strip accents from text (e.g., é -> e)."""
-
-        return "".join(c for c in unicodedata.normalize("NFD", text))
-
-    def sanitize_cmp(text: str):
-        """Strip CMP categories from text:
-        Other (Control, Format, Not Assigned, Private Use, Surrogate),
-        Mark (Spacing Combining, Enclosing, Nonspacing),
-        Punctuation (Connector, Dash, Close, Final quote, Initial quote, Other, Open).
-
-        We keep LNSZ (Letter, Number, Symbol, Separator). Separator is handled with regex \s.
-        """
-
-        # Other (C) removes LF
-        # Punctuation (P) removes dot
-
-        return "".join(
-            (
-                c
-                if unicodedata.category(c)[0] not in ["C", "M", "P"] or c in ["\n", "."]
-                else " "
-            )
-            for c in text
-        )
-
-    # Remove trash character sets
-    def sanitize_char_sets(text: str):
-        return "".join(
-            c if unicodeblock.blocks.of(c) not in ["HANGUL_JAMO", "CYRILLIC"] else " "
-            for c in text
-        )
-
-
-class Chars:
-    # Equivalent EXTENDED ARABIC-INDIC DIGIT
-    mappings_extended_arin_en_digit = [
-        ("۰", "0"),
-        ("۱", "1"),
-        ("۲", "2"),
-        ("۳", "3"),
-        ("۴", "4"),
-        ("۵", "5"),
-        ("۶", "6"),
-        ("۷", "7"),
-        ("۸", "8"),
-        ("۹", "9"),
-    ]
-
-    # Equivalent ARABIC-INDIC DIGIT
-    mappings_arin_en_digit = [
-        ("٠", "0"),
-        ("١", "1"),
-        ("٢", "2"),
-        ("٣", "3"),
-        ("٤", "4"),
-        ("٥", "5"),
-        ("٦", "6"),
-        ("٧", "7"),
-        ("٨", "8"),
-        ("٩", "9"),
-    ]
-
-    # Equivalent letters
-    mappings_ar_fa_letters = [
-        ("ە", "ه"),  # ARABIC LETTER AE, ARABIC LETTER HEH
-        ("ہ", "ه"),  # ARABIC LETTER HEH GOAL, ARABIC LETTER HEH
-        ("ٸ", "ی"),  # ARABIC LETTER HIGH HAMZA YEH, ARABIC LETTER FARSI YEH
-        ("ھ", "ه"),  # ARABIC LETTER HEH DOACHASHMEE, ARABIC LETTER HEH
-        ("ى", "ی"),  # ARABIC LETTER ALEF MAKSURA, ARABIC LETTER FARSI YEH
-        ("ں", "ن"),  # ARABIC LETTER NOON GHUNNA, ARABIC LETTER NOON
-        ("ے", "ی"),  # ARABIC LETTER YEH BARREE, ARABIC LETTER FARSI YEH
-        ("ﯼ", "ی"),  # ARABIC LETTER FARSI YEH ISOLATED FORM # NEW!
-        ("آ", "ا"),  # A kolah-dar
-        ("ي", "ی"),  # ARABIC LETTER YEH
-    ]
-
-    # Equivalent special characters
-    mappings_ar_ = [
-        ("؛", ";"),
-        ("٪", "%"),
-        ("²", "2"),
-        ("³", "3"),
-    ]
-
-    burn_chars_a = [
-        "\u200e",  # RLM
-        "\u200f",  # LRM
-        "\u200c",  # ZWNJ
-        "\u00ad",  # SOFT HYPHEN [SHY]
-        "\u2026",  # HORIZONTAL ELLIPSIS (re.sub doesn't pick up … so used hex value)
-        "®",
-        "©",
-        "™",
-        "(",
-        ")",
-        "/",
-        ",",
-        ":",
-        "[",
-        "]",
-        "«",
-        "»",
-        "<",
-        ">",
-        "'",
-        '"',
-        "#",
-        "*",
-        "،",  # ARABIC COMMA
-        "-",
-        "|",
-    ]
-
-    def get_mappings():
-        classvars = list(filter(lambda a: a.startswith("mappings_"), vars(Chars)))
-        mappings = []
-        for cv in classvars:
-            mappings.append(getattr(Chars, cv))
-        return mappings
-
-    def get_burn_chars():
-        classvars = list(filter(lambda a: a.startswith("burn_"), vars(Chars)))
-        burn_chars = []
-        for cv in classvars:
-            burn_chars += getattr(Chars, cv)
-        burn_chars = "".join(re.escape(c) for c in burn_chars)
-        return f"[{burn_chars}]"
-
-    def get_cjk():
-        return re.compile("[\u4e00-\u9fff]")
---
+++ yumbox/nlp/processors.py
@@ -0,0 +1,470 @@
+import os
+import re
+from typing import Literal
+
+import hazm
+import nltk
+import pandas as pd
+import unicodeblock.blocks
+import unicodeblock.sequence
+from nltk.corpus import stopwords
+from tqdm import tqdm
+
+from .chars import Chars, Unicode, UnicodeRanges
+from .tools import MapRed
+
+
+def sentence_normalizer(s: str):
+    """Sentence -> sentences normalized by unicode family.
+    Deprecated in favor of Preprocessor class."""
+    _o = "".join(s.split())
+
+    # TODO: REMOVES FARSI CHARACTERS
+    # import unicodedata
+    # import unicodeblock.sequence
+    s = " ".join([str(x) for x in unicodeblock.sequence.usplit(s)])
+
+    # TODO: Replace specs
+    # Replace v with volate
+    # Replace a with ampere
+    # Replace ... with ...
+
+    # TODO: hazm.word_tokenize splits based on family (en, fa, numeric)
+
+    _t = "".join(s.split())
+
+    if _o != _t:
+        print("WARNING: Split escaped some characters!")
+
+    return s
+
+
+def sentence_preprocessor(s: str, keep_lf=False) -> str:
+    """Sentence -> sentence.
+    Deprecated in favor of Preprocessor class."""
+
+    # Normalize
+    s = s.strip()
+    s = s.lower()
+
+    # Replace with equivalent characters
+    for mapping in Chars.get_mappings():
+        for x, y in mapping:
+            s = s.replace(x, y)
+
+    # Remove trash chars
+    s = re.sub(Chars.get_burn_chars(), " ", s)
+    s = re.sub(Chars.get_cjk(), " ", s)
+    s = Unicode.strip_accents(s)
+    s = Unicode.sanitize_cmp(s)
+    s = Unicode.sanitize_char_sets(s)
+
+    # Special preprocessor
+    s = product_title_preprocessor(s)
+
+    # Remove stop words
+    s = " ".join([w for w in s.split(" ") if w not in online_shop_stop_words()])
+
+    # Farsi normalizer
+    s = fa_sent_normalizer(s)
+
+    # English normalizer
+    s = en_sent_normalizer(s)
+
+    # Remove extra spaces
+    if keep_lf:
+        s = re.sub("[\t\r\n\v\f]", " ", s)
+        s = re.sub("[\t\r\n\v\f][\t\r\n\v\f]+", " ", s)
+    else:
+        # s = re.sub("\s", " ", s)
+        # s = re.sub("\s\s+", " ", s)
+        s = " ".join(s.split())
+
+    # Removed chars leave spaces
+    s = s.strip()
+
+    return s
+
+
+def column_preprocessor(arr: list[str], join_by=" ", appendlf=True) -> str:
+    arr = [a for a in arr if a]
+    text = join_by.join(arr)
+
+    text = sentence_preprocessor(text, keep_lf=True)
+
+    if text and appendlf:
+        text += "\n"
+
+    return text
+
+
+def debug_sentence_preprocessor(s: str) -> str:
+    import inspect
+    import re
+
+    space_pattern = re.compile("^\s+")
+
+    def inject_print(func):
+        source = inspect.getsource(func)
+        lines = source.splitlines()
+
+        # Keep the function definition line
+        modified_lines = [lines[0]]
+
+        # Loop through the rest of the lines (the body of the function)
+        for line in lines[1:]:
+            modified_lines.append(line)
+            if " = " in line:
+                spaces = re.search(space_pattern, line)[0]
+                modified_lines.append(spaces + f'print(""">>>{line.strip()}""")')
+                modified_lines.append(spaces + "print(s)")
+
+        # Join the modified lines back into a string
+        modified_code = "\n".join(modified_lines)
+
+        return modified_code
+
+    injected_sentence_preprocessor = inject_print(sentence_preprocessor)
+    injected_sentence_preprocessor = "\n".join(
+        [injected_sentence_preprocessor, f's = r"""{s}"""', "sentence_preprocessor(s)"]
+    )
+
+    exec(injected_sentence_preprocessor)
+
+
+# from nltk.corpus import wordnet
+# from nltk.stem import PorterStemmer, WordNetLemmatizer
+
+fa_stop_words = None
+en_stop_words = None
+
+
+def init_fa_stop_words():
+    global fa_stop_words
+    if fa_stop_words is None:
+        fa_stop_words = set(hazm.stopwords_list())
+
+
+def init_en_stop_words(use_custom=True):
+    global en_stop_words
+    if en_stop_words is None:
+        if not use_custom:
+            en_stop_words = list(stopwords.words("english"))
+            # en_stop_words = [s for s in en_stop_words if len(s) > 2]
+        else:
+            en_stop_words = custom_en_stop_words()
+
+
+def custom_en_stop_words():
+    with open(os.path.join(os.path.dirname(__file__), "stop_words.txt")) as fd:
+        esw = fd.readlines()
+    return [s.strip() for s in esw if s.strip()]
+
+
+def fa_sent_normalizer(text: str):
+    """Sentence -> word -> sentence.
+    Farsi tokenize, remove stop words."""
+    if fa_stop_words is None:
+        init_fa_stop_words()
+
+    # normalizer = hazm.Normalizer()  # !!! Changes English digit to Farsi digit
+    # stemmer = hazm.Stemmer()
+    # lemmatizer = hazm.Lemmatizer()
+
+    # Splits based on family (English, Farsi, numeric):
+    # word_tokenizer = hazm.word_tokenize
+    word_tokenizer = lambda s: s.split(" ")
+    sent_tokenizer = hazm.sent_tokenize
+
+    # Tokenize
+    sentenecs = sent_tokenizer(text)
+    procd_sentenecs = []
+    for sent in sentenecs:
+        words = word_tokenizer(sent)
+
+        # Preprocess
+        words = [w for w in words if w not in fa_stop_words]
+        # words = [normalizer.normalize(w) for w in words]
+        # words = [stemmer.stem(w) for w in words]
+        # words = [lemmatizer.lemmatize(w) for w in words]
+
+        # words = [w for w in words if " " not in w]
+
+        sent = " ".join(words)
+        procd_sentenecs.append(sent)
+
+    return "\n".join(procd_sentenecs)
+
+
+def en_sent_normalizer(text: str):
+    """Sentence -> word -> sentence.
+    English tokenize, remove stop words."""
+    if en_stop_words is None:
+        init_en_stop_words()
+
+    # stemmer = PorterStemmer()  # en_stemmer.stem()
+    # lemmatizer = WordNetLemmatizer()  # en_lemmatizer.lemmatize()
+
+    word_tokenizer = nltk.word_tokenize
+    sent_tokenizer = nltk.sent_tokenize
+
+    # Tokenize
+    sentences = sent_tokenizer(text)
+    procd_sentences = []
+    for sent in sentences:
+        words = word_tokenizer(sent)
+
+        # Preprocess
+        words = [w for w in words if w not in en_stop_words]
+        # words = [stemmer.stem(w) for w in words]
+
+        # Lemmatize
+        # words = [lemmatizer.lemmatize(w) for w in words]
+
+        # Lemmatize with part-of-speech (POS) tags
+        # words = [lemmatizer.lemmatize(word, pos="v") for word in words]  # 'v' for verbs
+
+        # Lemmatize with Wordnet
+        # words = [
+        #     lemmatizer.lemmatize(w, get_wordnet_pos(w))
+        #     for w in words
+        #     if lemmatizer.lemmatize(w, get_wordnet_pos(w)) != ""
+        # ]
+
+        # words = [w for w in words if " " not in w]
+
+        sent = " ".join(words)
+        procd_sentences.append(sent)
+
+    return "\n".join(procd_sentences)
+
+
+def __debug(arr: list) -> str:
+    RTL_CTRL_CHAR = "\u202b"
+    arr = [a for a in arr if a]
+    arr = [re.sub("\s", " ", a) for a in arr]
+    sentence = "\n".join(arr)
+    return sentence
+
+
+def product_title_preprocessor(text: str):
+    """Sentence -> comprehension -> sentence."""
+
+    # Keep words longer than 2 chars
+    # isnum = re.compile("^\d+$")
+    # text = " ".join([t for t in text.split(" ") if (len(t) > 2) or (re.match(isnum, t))])
+
+    # Remove text inside parantheses or brackets
+    # text = re.sub(r"\([^)]*\)|\[[^\]]*\]", "", text)
+
+    # Filter out characters not in ok_alphabet
+    # text = "".join([t for t in text if t in ok_alphabet])
+
+    return text
+
+
+def online_shop_stop_words():
+    """Returns one list of English and Farsi stopwords
+    Used both in preprocess and postprocess.
+    Preprocess: For inference on model.
+    Postprocess: For cleaning corpus."""
+
+    # Returns two lists of stopwords: English and Farsi
+    # return ["rohs"], ["خرید"]
+    return [
+        "rohs",
+        "خرید",
+        "فروش",
+        "عمده",
+        "برای",
+        "قیمت",
+        "طی تماس",
+        "تماس",
+        "بگیرید",
+        "جهت سفارش",
+        "سفارش",
+        "تلفنی",
+        "تلفن",
+        "های کپی",
+        "کپی",
+        "اصل",
+        "org",
+    ]
+
+
+def word_post_process(sentence: str, mr: MapRed, verbose=False) -> str:
+    """Sentence -> word -> sentence.
+    Remove len 1 words, long words, low req words, stop words."""
+
+    words = sentence.split(" ")
+
+    # TODO: Convert this to comprehension
+    pwords = []
+    for w in words:
+        # Remove len 1 words
+        if len(w) < 2:
+            verbose and print(f"skipping {w}")
+            continue
+
+        # 56 was the largest mfr_no word (split by space) from my inspection
+        if len(w) > 56:
+            verbose and print(f"skipping {w}")
+            continue
+
+        # Remove low freq words
+        if w in mr.lowfreq:
+            verbose and print(f"skipping {w}")
+            continue
+
+        # Remove stop words (ROHS)
+        # Check separately than appending to mr.lowfreq made it twice as fast
+        if w in online_shop_stop_words():
+            verbose and print(f"skipping {w}")
+            continue
+
+        pwords.append(w)
+
+    sentence = " ".join(pwords)
+
+    return sentence
+
+
+def char_post_process(text: str, mr: MapRed, verbose=False):
+    """Sentence -> sentence.
+    Remove low freq chars."""
+
+    # Remove low freq chars
+    verbose and print("Removing low frequency chars")
+    for c in mr.lowfreq:
+        verbose and print(f"Removing {c}")
+        text = text.replace(c, " ")
+
+    return text
+
+
+def analyze_chars(mr_word: MapRed, mr_char: MapRed):
+    """For every unique character, shows an example word.
+
+    Args:
+        mr_word (MapRed): MapRed instance for words.
+        mr_char (MapRed): MapRed instance for characters.
+    """
+
+    def _word_contains_char(words, char):
+        for word in words:
+            if char in word:
+                return word
+
+    words = mr_word.dictionary.keys()
+    mr_char.sort("desc")
+    for char, freq in mr_char.dictionary.items():
+        word = _word_contains_char(words, char)
+        print(char, freq, word)
+
+
+def analyze_topten(mr: MapRed):
+    """Sorts desc and asc and prints top 10 word/char -> freq.
+
+    Args:
+        mr (MapRed): map reduce by word or character level
+    """
+
+    mr.sort("desc")
+    keys = list(mr.dictionary.keys())[0:10]
+    t = {k: mr.dictionary[k] for k in keys}
+    print(t)
+
+    mr.sort("asc")
+    keys = list(mr.dictionary.keys())[0:10]
+    t = {k: mr.dictionary[k] for k in keys}
+    print(t)
+
+
+def analyze_head(df: pd.DataFrame, sort_key: Literal["len", "freq"]):
+    """Sorts MapRed dataframe by key specified in descending.
+
+    Args:
+        df (pd.DataFrame): MapRed dataframe
+        sort_key (str): Sort based on len or freq of word or char.
+    """
+
+    df["len"] = df["word"].apply(lambda x: len(x))
+    sdf = df.sort_values(by=[sort_key], ascending=False)
+
+    pd.set_option("display.max_columns", None)
+    pd.set_option("display.max_rows", None)
+    pd.set_option("display.width", None)
+    pd.set_option("display.max_colwidth", None)
+    pd.set_option("display.max_seq_item", None)
+
+    print(sdf.head(2000))
+
+
+def run_analysis(inpath: str):
+    regex = UnicodeRanges.regex_cjk_chars()
+
+    mr_word = MapRed()
+    mr_char = MapRed()
+    with open(inpath, "r", encoding="utf-8") as fd:
+        for l in tqdm(fd.readlines()):
+            l = l.strip()
+            for word in l.split(" "):
+                mr_word(word)
+                # bool(regex.search(word)) and print(l)
+                for char in word:
+                    mr_char(char)
+    analyze_topten(mr_word)
+    analyze_topten(mr_char)
+
+    df_word = mr_word.df
+    analyze_head(df_word, sort_key="len")
+
+    df_char = mr_char.df
+    df_char["block"] = df_char["word"].apply(lambda x: unicodeblock.blocks.of(x))
+    analyze_head(df_char, sort_key="freq")
+
+    analyze_chars(mr_word, mr_char)
+
+
+def run_post_process(inpath: str, outpath: str):
+    print("Reading dataset file...")
+    corpus = []
+    mr_word = MapRed()
+    mr_char = MapRed()
+    with open(inpath, "r", encoding="utf-8") as fd:
+        for l in tqdm(fd.readlines()):
+            l = l.strip()
+            corpus.append(l)
+            for word in l.split(" "):
+                mr_word(word)
+                for char in word:
+                    mr_char(char)
+
+    # Remove duplicate lines
+    corpus = list(set(corpus))
+
+    # Remove single word lines
+    # Remove 1-2 character(s) lines
+    corpus = [c for c in corpus if len(c) > 2 and len(c.split()) > 1]
+
+    print("Running word process...")
+    mr_word.set_lowfreq(10)  # 10 not good for isee dataset
+    procd = []
+    for l in tqdm(corpus):
+        p = word_post_process(l, mr_word)
+        procd.append(p)
+
+    print("Running character process...")
+    corpus = "\n".join(procd)
+    mr_char.set_lowfreq(100)
+    corpus = char_post_process(corpus, mr_char, verbose=True)
+
+    # Trash consecutive sequence of numbers
+    print("Removing consequitive sequence of digits...")
+    corpus = re.sub("(\d+ )+\d+", " ", corpus)
+    corpus = re.sub("\s\s+", " ", corpus)
+
+    print("Saving processed corpus...")
+    with open(outpath, "w", encoding="utf-8") as fd:
+        fd.write(corpus)
+    print("All done!")
